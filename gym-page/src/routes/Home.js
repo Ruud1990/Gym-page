acab577d44845b3af1c7b2c599d29046569f2f7d
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-10-24 18:17:15 +0200

    added home page

--- gym-page/src/routes/Home.js
+++ gym-page/src/routes/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import photo from '../assets/herobannerimage.jpg'
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <section className='hero'>
+          <div className='hero-center'>
+            <article className='hero-info'>
+              <h1>
+                Be Fit App <br />
+                Be healthy, smiling and sweaty
+              </h1>
+              <p>
+                Your encyclopedia to healthy lifestyle. We are trusted by thousands of people, be one of them.
+              </p>
+              <button className='btn'>Check exercises</button>
+            </article>
+            <article className='hero-images'>
+              <img src={photo} className='home-img' alt='woman in gym' />
+            </article>
+          </div>
+        </section>
+      );
 }
 
 export default Home
