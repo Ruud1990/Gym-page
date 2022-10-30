cff9a5d1e8b2127e8f06645d3bb06adfbd009454
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-10-30 20:04:42 +0100

    added navbar

--- gym-page/src/components/Navbar.js
+++ gym-page/src/components/Navbar.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import {FaBars } from 'react-icons/fa';
+import { social } from '../assets/data';
 
-const Navbar = ()  => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    }
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+          linksContainerRef.current.style.height = `${linksHeight}px`;
+        } else {
+          linksContainerRef.current.style.height = '0px';
+        }
+      }, [showLinks]);
+
+    return (
+        <nav className='navbar'>
+          <div className='nav-center'>
+          <div className='nav-header'>
+          <img src='' className='logo' alt='logo' />
+          <button className='nav-toggle' onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+            <div className='links-container' ref={linksContainerRef}>
+            <ul className='links' ref={linksRef}>
+              <li>
+              <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/AboutUs">About us</Link>
+              </li>
+              <li>
+              <Link to="/Exercises">ExerciseList</Link>
+              </li>
+              <li>
+              <Link to="/RecipesList">RecipesList</Link>
+              </li>
+              <li>
+              <Link to="/groceryList">groceryList</Link>
+              </li>
+            </ul>
+            </div>
+            <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+          </div>
+        </nav>
+  )};
 
 export default Navbar
+
+
+
+
--- gym-page/src/routes/Home.js
+++ gym-page/src/routes/Home.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import photo from '../assets/herobannerimage.jpg'
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
+  const navigate = useNavigate();
     return (
         <section className='hero'>
           <div className='hero-center'>
             <article className='hero-info'>
               <h1>
-                Be Fit App <br />
-                Be healthy, smiling and sweaty
+                Be Fit App
               </h1>
+              <h3>
+                Be healthy, smiling and sweaty
+              </h3>
               <p>
                 Your encyclopedia to healthy lifestyle. We are trusted by thousands of people, be one of them.
               </p>
-              <button className='btn'>Check exercises</button>
+              <button className='btn'  onClick={() => {
+            navigate('/Exercises');
+          }}>Check exercises</button>
             </article>
             <article className='hero-images'>
               <img src={photo} className='home-img' alt='woman in gym' />
