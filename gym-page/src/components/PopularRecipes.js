631130b74737d4aa0f70ec251b31e946396cfb4b
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-11-05 17:47:54 +0100

    added local storage for popular recipes section

--- gym-page/src/components/PopularRecipes.js
+++ gym-page/src/components/PopularRecipes.js
@@ -11,11 +11,20 @@ const PopularRecipes = () => {
     }, [])
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPES_KEY}&number=7`);
-        const data = await api.json();
-        setPopular(data.recipes);
-        console.log(data.recipes);
+        const check = localStorage.getItem('popular');
+
+        if(check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPES_KEY}&number=7`);
+            const data = await api.json();
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+            console.log(data.recipes);
+        }
     }
+
+
 
 
   return (
